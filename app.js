60ee964af95a918d571d54925c971cda5fe505c6
Martin Laurnagaray <mlaurnagaray04@gmail.com>
2025-02-07 16:54:58 -0300

    corrige el codigo

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ let listaAmigo = document.getElementById('listaAmigos');
 
 console.log(nuevoAmigo) ;
 
+function actualizarAmigo() {
+    //Limpia la lista de amigos antes de actualizarla
+    listaAmigo.innerHTML = '';
+
+    //Recorre el array de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        //Crea un nuevo elemento li
+        const li = document.createElement('li');
+        //Establece el texto del <li> como el nombre del amigo
+        li.textContent = amigos[i];
+        //Agregar el <li> a la lista <ul>
+        listaAmigo.appendChild(li);
+    }
+    return;
+}
+
 function agregarAmigo() {
     //Recibe el valor (nombre|string) enviado por el usuario
     nuevoAmigo = document.getElementById('amigoRecibido').value;
@@ -22,23 +38,13 @@ function agregarAmigo() {
         console.log(amigos);
         //listaAmigo.innerHTML = `amigo: ${nuevoAmigo}`;
 
-        //Limpia la lista de amigos antes de actualizarla
-        listaAmigo.innerHTML = '';
-
-        //Recorre el array de amigos
-        for (let i = 0; i < amigos.length; i++) {
-            //Crea un nuevo elemento li
-            const li = document.createElement('li');
-            //Establece el texto del <li> como el nombre del amigo
-            li.textContent = amigos[i];
-            //Agregar el <li> a la lista <ul>
-            listaAmigo.appendChild(li);
-        }
+        actualizarAmigo();
         
         console.log(`Amigo agregado: ${nuevoAmigo}`);
         console.log(typeof(nuevoAmigo));
     }
     limpiarCaja();
+    return;
 }
 
 //Limpia el campo de entrada como el principio
@@ -48,8 +54,4 @@ function limpiarCaja() {
         mensajeAdvertencia.remove();
     }
     return; 
-}
-
-function actualizarAmigos() {
-    //Seleccionar el espacio donde se colocarán los amigos
 }
